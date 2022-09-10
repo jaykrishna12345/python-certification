88ef4f776055dc554c8e0e4c7075e56fc1b393f1
jaykrishna12345 <108941785+jaykrishna12345@users.noreply.github.com>
2022-09-10 18:13:05 +0530

    Add files via upload

---
+++ interview_questions.py
@@ -0,0 +1,70 @@
+# Data types
+
+#  What values can the Boolean data type hold?
+ # Ans:   True and flase value 
+# what does it mean that python is a dynamically type language?
+  # Ans.pythone variable can be assigned to different type and changes type at will
+# what do we call it when we convert form one data type to anthore ?
+  # Ans.casting
+# what character being a single line comment ?
+  # Ans. #
+# what is the type of the following "7.1"?
+  # Ans. str.
+
+# opertors
+
+
+  # what are type of opertor are use in python?
+   # Ans. #.Arithmetic opertors
+          #.comparison opertors
+          #.pythone assignment oppertors
+          #.logical operators 
+          #.membership operators
+          #.identity operators
+  # when do you chooes a list over a tuple?
+    # Ans. simple tuple are immutable and list are not 
+
+# conditionl statement
+  
+  # what constitues "true" in python ?
+   # Ans. a true expression is any expresion that does not evaluate to 0,the empty list[],tuple(),dictonary{},or the object none or flase
+  # what are the three main conditional statement in python ?
+    # Ans. if,elif and else 
+  # How is collection membership determined ?
+    # Ans. using the in and not in operators 
+  # when are dictionaries considered equal ?
+    # Ans. if and only if their sorted list comapre equally
+  # when comparing two d^tes,what method is used?
+    # Ans. data.toordinal() otherwise,python would compare the dates by their object addrese
+
+
+
+# Looping statement 
+
+
+ # what keyword is used for looping ?
+  # Ans. for 
+ # what function can generate a list of numbers ?
+  # Ans. range 
+ # name the 2 keywords used for looping ?
+  # Ans. for, while 
+ # what keyword is used to skip back to the beginning of a loop ?
+  # Ans. continue
+ # use range to generate a list form 1 thourgh 5 ?
+  # Ans. range (1,6)
+
+
+
+# Functions 
+
+
+ # what function is used to accept user input ?
+  # Ans. input()
+ # what function is used to split user data into multiple values ?
+  # Ans. split
+ # you define you are creating a function with what keyword ?
+  # Ans. return
+ # To retrun multiple values from a function we separate value with ?
+  # Ans. commas 
+ # what function is called each time you creat a new object ?
+  # Ans. init 
